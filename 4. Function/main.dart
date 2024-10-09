27e43556fd07c5689f051fa75d3aeb1e6d390f97
ghozal.perdana02 <ghozal.perdana02@gmail.com>
2024-10-09 05:08:37 +0000

    perubahan

--- 4. Function/main.dart
+++ 4. Function/main.dart
@@ -2,14 +2,14 @@
 
 void main(){
     int hasilPenjumlahan = penjumlahan(3, 6, 4);
-    hasilPenjumlahan = hasilPenjumlahan + 2;
+    hasilPenjumlahan = hasilPenjumlahan + 2; //FUNGSI INI AKAN DI TAMBAHKAN 2 KE HASIL PENJUMLAHAN
     print("hasil Penjumlahan (main) $hasilPenjumlahan");
     
     pengurangan(4, 1);
 }
 
 int penjumlahan(int angka1, int angka2, int angka3){
-    int hasil = angka1 + angka2 + angka3;
+    int hasil = angka1 + angka2 + angka3; //FUNGSI INI AKAN MENGHASILKAN HASIL PENJUMLAHAN
     print("hasil penjumlahan adalah $hasil");
     return hasil;
 }
